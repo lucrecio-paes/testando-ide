0522cca0386e45ef2041c08394395187f2857cdd
Lucrecio Paes <lcpaes40@gmail.com>
2022-09-08 08:38:21 -0300

    class livro

--- src/br/com/primeiro/PrimeiroPrograma.java
+++ src/br/com/primeiro/PrimeiroPrograma.java
@@ -7,7 +7,46 @@ public class PrimeiroPrograma {
 //        System.out.println("hello");
 
         Gato gato = new Gato();
+        Livro livro = new Livro();
 
         System.out.println(gato);
+        System.out.println(livro);
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer anoLancamento;
+
+    public Livro() {
+    }
+
+    public Livro(String nome, Integer anoLancamento) {
+        this.nome = nome;
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(Integer anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", anoLancamento=" + anoLancamento +
+                '}';
     }
 }
