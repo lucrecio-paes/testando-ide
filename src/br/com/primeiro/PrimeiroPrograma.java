2731138f7da179293659bdf54b28d92dfe3d8430
Lucrecio Paes <lcpaes40@gmail.com>
2022-09-08 08:29:22 -0300

    first commit

---
+++ src/br/com/primeiro/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.primeiro;
+
+import br.com.primeiro.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+//        System.out.println("hello");
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+    }
+}
